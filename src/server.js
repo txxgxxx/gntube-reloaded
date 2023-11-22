914d3ece8bd793dd65c0a3bb7b6cb665e28d37ca
txxgxxx <taegun1020@naver.com>
2023-11-23 07:52:28 +0900

    3.7~3.10 recap

--- src/server.js
+++ src/server.js
@@ -4,37 +4,19 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(` ${req.method} ${req.url}`);
-    next();
-}
+const home = (req, res) => {
+    console.log("I will respond.");
+    return res.send("hello");
+};
+const login = (req, res) => {
+    return res.send("login");
+};
 
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue.")
-    next();
-}
+app.get("/", home);
+app.get("/login", login);
 
-const handleHome = (req, res) => {
-    return res.send("<h1>I still love you.</h1>");
-}
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.");
-}
-
-app.use(logger);
-app.use(privateMiddleware);
-
-app.get("/",handleHome);
-app.get("/protected", handleProtected);
-
-
-
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+const handleListening = () => 
+console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
 
 
