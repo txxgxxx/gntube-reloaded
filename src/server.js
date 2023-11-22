d958262c85b534f69626435a44c4466ab7fbbf63
txxgxxx <taegun1020@naver.com>
2023-11-23 03:34:52 +0900

    3.5 middleware part one

--- src/server.js
+++ src/server.js
@@ -4,17 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
-    return res.send("I still love you.");
+    return res.send("<h1>I still love you.</h1>");
 }
 
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-}
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 
 
