e3838f53a31421095b60d35589cb92dd88d4f917
txxgxxx <taegun1020@naver.com>
2023-11-23 03:52:48 +0900

    3.6 middleware part Two

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(` ${req.method} ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.")
     next();
 }
 
@@ -13,8 +22,15 @@ const handleHome = (req, res) => {
     return res.send("<h1>I still love you.</h1>");
 }
 
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
 
-app.get("/", gossipMiddleware, handleHome);
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/",handleHome);
+app.get("/protected", handleProtected);
 
 
 
