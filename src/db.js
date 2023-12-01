bedfcca8b67841393f06eaa34a744b566843c5e9
txxgxxx <taegun1020@naver.com>
2023-12-01 15:03:35 +0900

    6.8 Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/gntube");
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
+
+// error는 여러번 일어날 수 있으니 on으로, 커넥팅은 한 번만 되니까 once로.
+db.on("error", handleError);
+db.once("open", handleOpen);
+
+
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+
 
 const PORT = 4000;
 
