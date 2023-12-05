370281f11ec86e93ada8e1f8636251bc59126c9b
txxgxxx <taegun1020@naver.com>
2023-12-05 23:20:56 +0900

    6.20 Edit Video part Two
    + hashtag #과 word 사이 공백 제거

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,53 @@
 import Video from "../models/video";
 
 export const home = async (req, res) => {
-    const videos = await Video.find({});
-    console.log(videos);
-    return res.render("home", { pageTitle: "Home", videos });
+  const videos = await Video.find({});
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
-    const { id } = req.params;
-    const video = await Video.findById(id);
-    res.render("watch", {pageTitle: video.title, video});
-}
-export const getEdit = (req,res) => {
-    const { id } = req.params;
-    res.render("edit", {pageTitle: `Editing`, });
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  res.render("watch", { pageTitle: video.title, video });
+};
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
-    const { id } = req.params;
-    const { title } = req.body;
-    return res.redirect(`/videos/${id}`);
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
-    return res.render("upload", {pageTitle: "Upload Video"});
-}
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
 
 export const postUpload = async (req, res) => {
-    const { title, description, hashtags } = req.body;
-    try {
-        await Video.create({
-            title,
-            description,
-            hashtags: hashtags.split(",").map((word) => !word.trim().startsWith("#") ? `#${word}` : word.trim()),
-        });
-        return res.redirect("/");
-    } catch(error){
-        return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message,});
-    }
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags
+        .split(",")
+        .map((word) =>
+          !word.trim().startsWith("#") ? `#${word.trim()}` : word.trim()
+        ),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
