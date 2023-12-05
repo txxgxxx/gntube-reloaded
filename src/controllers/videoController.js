40651736760f722ff2344145a59d4b56c7eba2d7
txxgxxx <taegun1020@naver.com>
2023-12-06 02:39:47 +0900

    6.25 Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,6 @@ import Video from "../models/video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -56,4 +55,10 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
 };
