f7fdbf68a6e3fd5f5e000b54a705b94b219efb76
txxgxxx <taegun1020@naver.com>
2023-12-08 15:22:00 +0900

    7.8 Sessions and Cookies part Two

--- src/server.js
+++ src/server.js
@@ -21,6 +21,18 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}/n${req.session.potato}`);
+});
+
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
