9e7fce74877d41253dec06d01154f584877e8a7f
txxgxxx <taegun1020@naver.com>
2023-12-10 17:01:15 +0900

    7.17 Github Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,6 +58,21 @@ export const postLogin = async (req, res) => {
   req.session.user = user;
   return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "2fffa16999207bba8648",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("remove User");
 export const logout = (req, res) => res.send("Log Out");
