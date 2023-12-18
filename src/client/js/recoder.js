a60f6a278478ab438abf150e8e01142ec3d29b2e
txxgxxx <taegun1020@naver.com>
2023-12-19 04:33:52 +0900

    13.2 Recording Video

--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -1,12 +1,41 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("recording done.");
+    console.log(event);
+    console.log(event.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
