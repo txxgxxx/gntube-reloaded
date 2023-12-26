c62747749a087f043427f8232125ce7e926e9ba5
txxgxxx <taegun1020@naver.com>
2023-12-26 22:35:18 +0900

    15.0 Installation (Flash Messages)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,6 +26,7 @@ export const getEdit = async (req, res) => {
     return res.status(404).res.render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash("error", "Not authorized");
     return res.status(403).redirect("/");
   }
   res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
@@ -41,6 +42,7 @@ export const postEdit = async (req, res) => {
     return res.status(404).res.render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash("error", "You are not the owner of the video.");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
@@ -94,6 +96,7 @@ export const deleteVideo = async (req, res) => {
     return res.status(404).res.render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash("error", "Not authorized");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndDelete(id);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -25,6 +26,7 @@ app.use(
   })
 );
 
+app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/goods", express.static("assets"));
