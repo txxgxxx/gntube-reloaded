41605e06b9cdea04a39553f162b0591c0943cf5b
txxgxxx <taegun1020@naver.com>
2023-12-28 12:34:02 +0900

    16.4 API Route part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -133,5 +133,6 @@ export const registerView = async (req, res) => {
 export const createComment = (req, res) => {
   console.log(req.params);
   console.log(req.body);
+  console.log(req.body.text);
   res.end();
 };
